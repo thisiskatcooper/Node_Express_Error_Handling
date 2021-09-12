67cba1d2230a072eba39777cb3b2b6e05b1fdcd9
Katherine Cooper <40881272+thisiskatcooper@users.noreply.github.com>
2021-09-11 17:19:41 -0700

    Add files via upload
    Initial file upload and commit.

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const { NODE_ENV = "development" } = process.env;
+const express = require("express");
+const app = express();
+
+// Routes
+app.get("/send/:message", (req, res, next) => {
+  const message = req.params.message;
+  if (message.length < 3) { next("An error occurred: Your message is too short!");                   }
+  else {
+    res.send(`Your message: ${message}`);
+  }
+});
+
+// Error Handling
+app.use((req, res, next) => {
+  res.send("An error occurred: Could not find route.")
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.send(err);
+});
+
+module.exports = app;
